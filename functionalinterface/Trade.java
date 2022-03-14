84cd114b9efc7f8acc613749a3f8a37e24968c9d
Hanmant12 <98298770+Hanmant12@users.noreply.github.com>
2022-03-14 20:12:45 +0530

    Add files via upload

---
+++ functionalinterface/Trade.java
@@ -0,0 +1,9 @@
+package functionalinterface;
+
+public class Trade {
+
+	public Trade(int i, String string, int j, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ functionalinterface/example.java
@@ -0,0 +1,22 @@
+package functionalinterface;
+//
+//public void testPreJava8()
+//{
+//	  IAddable<Trade> tradeMerger = new IAddable<Trade>() {
+//	  @Override
+//	    public Trade add(Trade t1, Trade t2) {
+//	      t1.setQuantity(t1.getQuantity() + t2.getQuantity());
+//	      return t1;
+//	    }
+//	  };
+//	}
+//public class example {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		 
+//		Trade t1 = new Trade(1, "GOOG", 12000, "NEW");
+//		Trade t2 = new Trade(2, "GOOG", 24000, "NEW");
+//	}
+//
+//}
